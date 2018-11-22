a4a1fd927b06998cd20f6dd9272564fac3eeef73
Microvoice <Microvoice@DESKTOP-PJ10M2P>
2018-11-22 15:55:00 +0100

    commit2

--- IntermediateTest_A/src/test/java/it/unical/ingsw2018/IntermediateTest_A/KnapsackTest.java
+++ IntermediateTest_A/src/test/java/it/unical/ingsw2018/IntermediateTest_A/KnapsackTest.java
@@ -25,10 +25,10 @@ public class KnapsackTest {
 	@Test
 	public void aggDueInserimento() {
 		knapsack.addItem(new Item(10, 2));
-		knapsack.addItem(new Item(20, 3));
+		knapsack.addItem(new Item(5, 3));
 
 		assertEquals(5, knapsack.getCurrentValue());
-		assertEquals(30, knapsack.getCurrentWeight());
+		assertEquals(15, knapsack.getCurrentWeight());
 	}
 
 	
@@ -36,14 +36,14 @@ public class KnapsackTest {
 	public void aggTerzoERimuovoUno() {
 		Item item = new Item(10, 2);
 		Item item1 = new Item(20, 3);
-		Item item2 = new Item(30, 4);
+		Item item2 = new Item(5, 4);
 
 		knapsack.addItem(item);
 		knapsack.addItem(item1);
 		knapsack.addItem(item2);
 
 		assertEquals(9, knapsack.getCurrentValue());
-		assertEquals(60, knapsack.getCurrentWeight());
+		assertEquals(35, knapsack.getCurrentWeight());
 
 		knapsack.removeItem(item2);
 		assertEquals(5, knapsack.getCurrentValue());
@@ -61,8 +61,8 @@ public class KnapsackTest {
 	public void bestItemFunziona() {
 		Item i = new Item(10, 2);
 		Item i1 = new Item(20, 3);
-		Item i2 = new Item(30, 30);
-		Item i3 = new Item(40, 3);
+		Item i2 = new Item(15, 30);
+		Item i3 = new Item(15, 3);
 
 		knapsack.addItem(i1);
 		knapsack.addItem(i2);
