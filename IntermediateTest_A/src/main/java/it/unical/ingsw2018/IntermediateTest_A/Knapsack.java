3233a5190f90086f50ac6147750e553f78f171b4
Alessio Portaro <alecioportaro@gmail.com>
2018-11-22 15:31:46 +0100

    commit PortaroAlessio176231

--- IntermediateTest_A/src/main/java/it/unical/ingsw2018/IntermediateTest_A/Knapsack.java
+++ IntermediateTest_A/src/main/java/it/unical/ingsw2018/IntermediateTest_A/Knapsack.java
@@ -1,5 +1,6 @@
 package it.unical.ingsw2018.IntermediateTest_A;
 
+import java.util.Comparator;
 import java.util.TreeSet;
 
 public class Knapsack 
@@ -17,7 +18,13 @@ public class Knapsack
 		this.capacity = capacity;
 		this.currentWeight = 0;
 		this.currentValue = 0;
-		this.items = new TreeSet<>();
+		this.items = new TreeSet<>(new Comparator<Item>() {
+
+			@Override
+			public int compare(Item o1, Item o2) {
+				return 1;
+			}
+		});
 	}
 
 	
@@ -41,6 +48,44 @@ public class Knapsack
 		items.clear();
 		currentValue = 0;
 		currentWeight = 0;
+	}
+	
+	public void addItem(Item item) {
+		if(currentWeight+item.getWeight()>capacity)
+			throw new KnapsackInsertionError();
+		System.out.println(items.add(item));
+		currentWeight+=item.getWeight();
+		currentValue+=item.getValue();
+	}
+	
+	public void removeItem(Item item) {
+		items.remove(item);
+		currentWeight-=item.getWeight();
+		currentValue-=item.getValue();
+	}
+	
+	public Item getBestItem() {
+		int bestValue=0;
+		Item bestItem = null;
+		boolean firstTry=true;
+		for(Item i : items) {
+			int currentValue1 = i.getValue()/i.getWeight();
+			if(firstTry) {
+				firstTry=false;
+				bestItem=i;
+				bestValue = currentValue1;
+				continue;
+			}
+			
+			if(currentValue1>bestValue) {
+				bestItem=i;
+				bestValue=currentValue1;
+			}
+		}
+		return bestItem;
+		//Per restituire l'elemento migliore in tempo costante basterebbe verificare 
+		//a tempo di inserimento il rapporto dell'elemento da inserire e tenere come radice dell'albero
+		//sempre l'elemento migliore, ma non c'è tempo di studiare la classe treeset
 	}
     
 }
--- IntermediateTest_A/src/test/java/it/unical/ingsw2018/IntermediateTest_A/KnapsackTest.java
+++ IntermediateTest_A/src/test/java/it/unical/ingsw2018/IntermediateTest_A/KnapsackTest.java
@@ -1,7 +1,12 @@
 package it.unical.ingsw2018.IntermediateTest_A;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Random;
+
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class KnapsackTest {
 	
@@ -15,5 +20,57 @@ public class KnapsackTest {
 	@Before
 	public void reset() {
 		knapsack.reset();
+	}
+	
+	@Test
+	public void after2insert() {
+		Item i1 = new Item(5, 5);
+		Item i2 = new Item(4, 5);
+		knapsack.addItem(i1);
+		knapsack.addItem(i2);
+		System.out.println("W : "+knapsack.getCurrentWeight()+" V : "+knapsack.getCurrentValue());
+		assertEquals(9, knapsack.getCurrentWeight());
+		assertEquals(10, knapsack.getCurrentValue());
+	}
+	
+	@Test
+	public void after3insertAnd1Remove() {
+		Item i1 = new Item(5, 5);
+		Item i2 = new Item(4, 5);
+		Item i3 = new Item(3, 3);
+		knapsack.addItem(i1);
+		knapsack.addItem(i2);
+		knapsack.addItem(i3);
+		knapsack.removeItem(i2);
+		assertEquals(8, knapsack.getCurrentValue());
+		assertEquals(8, knapsack.getCurrentWeight());
+	}
+	
+	@Test(expected=KnapsackInsertionError.class)
+	public void exceptioner() {
+		Item big = new Item(101, 1);
+		knapsack.addItem(big);
+	}
+	
+	@Test
+	public void bestItemTester() {
+		Item i1 = new Item(5, 5);
+		Item i2 = new Item(4, 5);
+		Item i3 = new Item(3, 3);
+		Item best = new Item(5,100);
+		knapsack.addItem(i1);
+		knapsack.addItem(i2);
+		knapsack.addItem(i3);
+		knapsack.addItem(best);
+		System.out.println(knapsack.getBestItem().getValue());
+		assertEquals(best, knapsack.getBestItem());
+	}
+	
+	@Test(timeout = 1000)//1 secondi = 1000 milli
+	public void performanceTest() {
+		Knapsack ks = new Knapsack(Integer.MAX_VALUE);
+		Random rand = new Random();
+		for(int i=0;i<1000;i++)
+			ks.addItem(new Item(rand.nextInt(),rand.nextInt()));
 	}
 }
