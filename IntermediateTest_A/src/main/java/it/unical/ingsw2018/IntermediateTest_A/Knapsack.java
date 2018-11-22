24d4224562c83147cf82aceafb26b3a47605fe9a
Giuseppe Oliva <giuseppeoliva1997@gmail.com>
2018-11-22 14:55:56 +0100

    Implementazione

--- IntermediateTest_A/src/main/java/it/unical/ingsw2018/IntermediateTest_A/Knapsack.java
+++ IntermediateTest_A/src/main/java/it/unical/ingsw2018/IntermediateTest_A/Knapsack.java
@@ -2,25 +2,27 @@ package it.unical.ingsw2018.IntermediateTest_A;
 
 import java.util.TreeSet;
 
-public class Knapsack 
-{
-  
-	private int capacity;
-    
-	private TreeSet<Item> items;
-	
-	private int currentWeight;
+public class Knapsack {
 
+	private int capacity;
+	private TreeSet<Item> items;
+	private int currentWeight;
 	private int currentValue;
 	
+	//Best item in tempo costante
+	private Item bestItem;
+	private float bestItemRatio;
+
 	public Knapsack(int capacity) {
 		this.capacity = capacity;
 		this.currentWeight = 0;
 		this.currentValue = 0;
 		this.items = new TreeSet<>();
+		
+		//Di default = 0, il primo (e unico) item è il primo bestItem
+		bestItemRatio = 0;
 	}
 
-	
 	public int getCurrentValue() {
 		return currentValue;
 	}
@@ -36,11 +38,38 @@ public class Knapsack
 	public int getCurrentWeight() {
 		return currentWeight;
 	}
-	
+
 	public void reset() {
 		items.clear();
 		currentValue = 0;
 		currentWeight = 0;
 	}
-    
+	
+	public void addItem(Item item) {
+		if (currentWeight + item.getWeight() > capacity)
+			throw new KnapsackInsertionError();
+		else {
+			items.add(item);
+			currentWeight += item.getWeight();
+			currentValue += item.getValue();
+			
+			if ((item.getValue() * 1.0f) / item.getWeight() > bestItemRatio) {
+				bestItem = item;
+				bestItemRatio = (item.getValue() * 1.0f) / item.getWeight();
+			}
+		}
+	}
+	
+	public void removeItem(Item item) {
+		if (!items.contains(item)) return;
+		
+		currentWeight -= item.getWeight();
+		currentValue -= item.getValue();
+		items.remove(item);
+	}
+	
+	public Item getBestItem() {
+		return bestItem;
+	}
+
 }
